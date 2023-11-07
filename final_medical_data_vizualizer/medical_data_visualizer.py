b0a77ae1ac2b425ff633d8a28d49f3115167774e
MrSz-84 <126359057+MrSz-84@users.noreply.github.com>
2023-11-07 22:28:53 +0100

    Exercise medical data visualizer - first fig

---
+++ final_medical_data_vizualizer/medical_data_visualizer.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import data
+df = pd.read_csv('medical_examination.csv')
+
+# Add 'overweight' column
+df['overweight'] = df[['weight', 'height']].apply(lambda x: 1 if (x[0] / (x[1]/100)**2) > 25 else 0, axis=1)
+
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 1 if x > 1 else 0)
+df['gluc'] = df['gluc'].apply(lambda x: 1 if x > 1 else 0)
+
+
+# Draw Categorical Plot
+def draw_cat_plot():
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+
+
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index()
+    df_cat.columns = ['cardio', 'variable', 'value', 'counts']
+
+    # Draw the catplot with 'sns.catplot()'
+    cat_plot = sns.catplot(data=df_cat, x='variable', y='counts', hue='value', kind='bar', height=6, col='cardio')
+    cat_plot.set_axis_labels("variable", 'total')
+
+
+    # Get the figure for the output
+    fig = cat_plot
+
+
+    # Do not modify the next two lines
+    fig.savefig('catplot.png')
+    return fig
+
+
+# Draw Heat Map
+def draw_heat_map():
+    # Clean the data
+    df_heat = None
+
+    # Calculate the correlation matrix
+    corr = None
+
+    # Generate a mask for the upper triangle
+    mask = None
+
+
+
+    # Set up the matplotlib figure
+    fig, ax = None
+
+    # Draw the heatmap with 'sns.heatmap()'
+
+
+
+    # Do not modify the next two lines
+    fig.savefig('heatmap.png')
+    return fig
+
